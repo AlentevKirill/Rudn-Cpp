2762bba2b290da243de587f50951941d45a61643
AlentevKirill <76614092+AlentevKirill@users.noreply.github.com>
2025-09-13 11:33:27 +0300

    Add files via upload

---
+++ hw1_calculator/cmake-build-debug/CMakeFiles/hw1_calculator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw1_calculator.dir/first_hw_calculator.cpp.o"
+  "hw1_calculator.exe"
+  "hw1_calculator.pdb"
+  "libhw1_calculator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw1_calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hw1_calculator/first_hw_calculator.h
@@ -0,0 +1,14 @@
+//
+// Created by Kirill on 26.11.2024.
+//
+
+#ifndef SECOND_TRY_CPP_FIRST_HW_CALCULATOR_H
+#define SECOND_TRY_CPP_FIRST_HW_CALCULATOR_H
+
+
+class first_hw_calculator {
+
+};
+
+
+#endif //SECOND_TRY_CPP_FIRST_HW_CALCULATOR_H
